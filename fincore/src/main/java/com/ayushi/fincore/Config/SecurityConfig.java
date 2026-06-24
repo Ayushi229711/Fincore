52188f6a846f84508dd06fa36a9102e7db2dd092
Ayushi Sachan <ayushisachan229@gmail.com>
2026-06-24 17:42:25 +0530

    updated

--- fincore/src/main/java/com/ayushi/fincore/Config/SecurityConfig.java
+++ fincore/src/main/java/com/ayushi/fincore/Config/SecurityConfig.java
@@ -25,7 +25,9 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs",
-                                "/v3/api-docs/**"
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
